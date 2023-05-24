5121a712f44b58a945e7fbe55468732b73dc0d2e
A-Sister <92495146+A-Sister@users.noreply.github.com>
2023-05-24 18:45:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+score_leftWrist = 0;
+score_rightWrist = 0;
+song_status1 = "";
+song_status2 = "";
+
+
+function preload() {
+    song1 = loadSound("music2.mp3");
+    song2 = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.position(670,300);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log(results);
+        score_leftWrist = results[0].pose.keypoints[9].score;
+        score_rightWrist = results[0].pose.keypoints[10].score;
+        console.log("Right Wrist Score "+ score_rightWrist+" Left Wrist Score: "+score_leftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWrist X - "+leftWristX+" leftWrist Y - "+leftWristY+" rightWrist X - " +rightWristX+" rightWrist Y - "+rightWristY);
+    }
+}
+
+function draw() {
+    image(video,0,0,600,500);
+
+    song_status1 = song1.isPlaying();
+    song_status2 = song2.isPlaying();
+
+    fill("#990000");
+    stroke("#990000");
+
+    if(score_rightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+
+         if(song_status2 == false) {
+             song2.play();
+             document.getElementById("name").innerHTML = "Song Playing: Harry Potter Theme Song";
+         }
+    }
+
+    if(score_leftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+        if(song_status1 == false){
+            song1.play();
+            document.getElementById("name").innerHTML = "Song Playing:  Peter Pan Song";
+        }
+    }
+}
+
